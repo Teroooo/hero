b42fdb4084f3e90ad9a10c0cd59a9e29713a3213
Terooo <up202204971@up.pt>
2023-10-06 22:53:20 +0100

    Creating the game

---
+++ src/main/java/Game.java
@@ -0,0 +1,31 @@
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Game {
+    Screen screen;
+
+    public Game() throws IOException {
+        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+        screen = new TerminalScreen(terminal);
+        screen.setCursorPosition(null); // we don't need a cursor
+        screen.startScreen(); // screens must be started
+        screen.doResizeIfNecessary(); // resize screen if necessary
+    }
+
+    public void draw() throws IOException {
+            screen.setCursorPosition(null); // we don't need a cursor
+            screen.startScreen(); // screens must be started
+            screen.doResizeIfNecessary(); // resize screen if necessary
+    }
+
+    public void run() throws IOException {
+
+            draw();
+
+    }
+
+}
