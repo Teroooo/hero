3105972bd4abe4301148df9bec86d949aefff9fd
Terooo <up202204971@up.pt>
2023-10-07 00:13:55 +0100

    Reading keystrokes

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,6 @@
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -6,26 +9,56 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Game {
-    Screen screen;
+    private Screen screen;
+    private int y=10;
+    private int x=10;
 
     public Game() throws IOException {
-        Terminal terminal = new DefaultTerminalFactory().createTerminal();
-        screen = new TerminalScreen(terminal);
-        screen.setCursorPosition(null); // we don't need a cursor
-        screen.startScreen(); // screens must be started
-        screen.doResizeIfNecessary(); // resize screen if necessary
-    }
-
-    public void draw() throws IOException {
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            screen = new TerminalScreen(terminal);
             screen.setCursorPosition(null); // we don't need a cursor
             screen.startScreen(); // screens must be started
             screen.doResizeIfNecessary(); // resize screen if necessary
+            screen.clear();
+            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+            screen.refresh();
+    }
+
+    public void draw() throws IOException {
+            screen.clear();
+            screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+            screen.refresh();
+    }
+
+    private void processKey(KeyStroke key) throws IOException {
+            if (key.getKeyType() == KeyType.ArrowUp) {
+                y--;
+                draw();
+            }
+            if (key.getKeyType() == KeyType.ArrowDown) {
+                y++;
+                draw();
+
+            }
+            if (key.getKeyType() == KeyType.ArrowRight) {
+                x++;
+                draw();
+            }
+            if (key.getKeyType() == KeyType.ArrowLeft) {
+                x--;
+                draw();
+            }
+
     }
 
     public void run() throws IOException {
-
-            draw();
-
+        while(true) {
+            KeyStroke key = screen.readInput();
+            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') {
+                screen.close();
+                break;
+            }
+            processKey(key);
+        }
     }
-
 }
