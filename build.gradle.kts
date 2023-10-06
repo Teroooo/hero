84e927feb92309b3901284fe86646408ae79535b
Terooo <up202204971@up.pt>
2023-10-06 22:10:20 +0100

    Importing Lanterna

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.googlecode.lanterna", "lanterna", "3.1.1")
 }
 
 tasks.test {
