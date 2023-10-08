2d7346bd3b9bfc824d822d02b214e72af70537b2
Terooo <up202204971@up.pt>
2023-10-08 22:32:00 +0100

    Walls

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -1,26 +1,25 @@
 import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.Terminal;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Arena {
     private int width;
     private int height;
-    Hero hero;
+    private Hero hero;
+    private List<Wall> walls;
 
     public Arena(int width, int height) {
         this.width = width;
         this.height = height;
 
         hero = new Hero(width / 2, height / 2);
+        this.walls = createWalls();
     }
 
     void processKey(KeyStroke key) {
@@ -42,6 +41,9 @@ public class Arena {
         graphics.setBackgroundColor(TextColor.Factory.fromString("#FFFFAA"));
         graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
         hero.draw(graphics);
+
+        for (Wall wall : walls)
+            wall.draw(graphics);
     }
 
 
@@ -57,5 +59,18 @@ public class Arena {
         if (position.getY() > height - 1) return false;
 
         return true;
+    }
+
+    private List<Wall> createWalls() {
+        List<Wall> walls = new ArrayList<>();
+        for (int c = 0; c < width; c++) {
+            walls.add(new Wall(c, 0));
+            walls.add(new Wall(c, height - 1));
+        }
+        for (int r = 1; r < height - 1; r++) {
+            walls.add(new Wall(0, r));
+            walls.add(new Wall(width - 1, r));
+        }
+        return walls;
     }
 }
---
+++ src/main/java/Wall.java
@@ -0,0 +1,26 @@
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+import java.util.List;
+
+public class Wall {
+    private List<Wall> walls;
+    private Position position;
+
+    public Wall(int x, int y) {
+        position = new Position(x, y);
+    }
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public void draw(TextGraphics graphics) {
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#FF5722"));
+        graphics.putString(new TerminalPosition(position.getX(), position.getY()), " ");
+    }
+}
